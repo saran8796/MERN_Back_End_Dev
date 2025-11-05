f9880262ed786ac9df3d5c329e16d54f353e6c99
Saran <sarankarthick2011@gmail.com>
2025-11-05 07:12:37 +0530

    add day 1 task

--- NodeJS/Tasks/Day 1/leo.js
+++ NodeJS/Tasks/Day 1/leo.js
@@ -1,9 +1,19 @@
-const leo = (charA, charB = "") => {
-  return charA + charB;
+const leo = (charA, charB, charC, charD) => {
+  return `\n\nMovie Leo: \n\n ${charA} (Thalapathy Vijay) lives as ${charB}, a peaceful café owner in Himachal Pradesh, hiding from his dark past as a ruthless gangster and drug-lord. He tries to keep his family safe, but trouble finds him when old enemies, led by ${charC} and ${charD}, come searching for ${charA}.\n\n`;
 };
 
 const scene1 = (charA, charB) => {
-  return leo(charA, charB);
+  return `Scene1: \n\nI have done nothing good to be remembered as ${charA} and that anyone who knows my identity must die or already be dead, recalling that he admitted the truth to ${charB} before killing him.\n\n`;
+};
+
+const scene2 = (charA) => {
+  return `Scene2: \n\nThe caller says he knows ${charA}’s true identity and contends that destroying his family's factory will not make society drug-free, imploring him to join his mission and correctly surmising that ${charA} might have recognized him by now.\n\n`;
+};
+
+const scene3 = (charA, charB, charC) => {
+  return `Scene3: \n\nAfter killing ${charC}, ${charA}/${charB} sets fire to the Das & Co factory, erasing all traces of his criminal past. He declares: \n I've done nothing good to be remembered as ${charA}. Anyone who knows my real identity must be dead or will die by my hands. My life is now to protect my family, not live as ${charA} anymore.\n\n`;
 };
 
 export default leo;
+
+export { scene1, scene2, scene3 };
--- NodeJS/Tasks/Day 1/main.js
+++ NodeJS/Tasks/Day 1/main.js
@@ -1,3 +1,6 @@
-import add from "./add.js";
+import leo, { scene1, scene2, scene3 } from "./leo.js";
 
-console.log(add(5, 6, 5));
+console.log(leo("Leo Das", "Parthiban", "Antony Das", "Harold Das"));
+console.log(scene1("Leo Das", "Antony Das"));
+console.log(scene2("Parthiban"));
+console.log(scene3("Leo Das", "Parthiban", "Harold Das"));
